d635f4973ab3fd86000675af63db0b1ab6b29fb5
Daniel Reiling <daniel.reiling@gmail.com>
2021-02-04 16:00:08 -0600

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Daniel Reiling on 2/4/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var imageURL: URL
+}
