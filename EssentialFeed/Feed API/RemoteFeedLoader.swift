815f7e98d2e1b910501393002c826d32e96dd00b
Daniel Reiling <daniel.reiling@gmail.com>
2021-02-04 17:39:54 -0600

    Refactored production code into EssentialFeed target

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Daniel Reiling on 2/4/21.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
