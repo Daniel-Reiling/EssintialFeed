93ef2788f93c305b23d6f82354fe385732761d26
Daniel Reiling <daniel.reiling@gmail.com>
2021-02-18 19:20:12 -0600

    Updated RemoteFeedLoader error type confirm to The Feedloader error type in preparation to conform RemoteFeedLoader to FeedLoader protocol

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 
-enum LoadFeedResult {
+public enum LoadFeedResult<Error: Swift.Error> {
     case success([FeedItem])
-    case error(Error)
+    case failure(Error)
 }
 
+extension LoadFeedResult: Equatable where Error: Equatable {}
+
 protocol FeedLoader {
-    func load(completion: @escaping (Result<LoadFeedResult, Error>) -> Void)
+    associatedtype Error: Swift.Error
+    func load(completion: @escaping (Result<LoadFeedResult<Error>, Error>) -> Void)
 }
